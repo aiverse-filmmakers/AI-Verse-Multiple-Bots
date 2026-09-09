5ca4cbd8c0d257e5b265c7b3270a2c04c9ac313b
AI-VERSE <hello@bogdanpascal.com>
2026-09-10 01:05:17 +0300

    Phase 2.10: export TeamRunCleanup

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export * from "./runner.js";
 export * from "./runtime.js";
 export * from "./store.js";
 export * from "./supervisor.js";
+export * from "./team-run-cleanup.js";
 export * from "./team-run-discussion.js";
 export * from "./team-run-disagreement.js";
 export * from "./team-run-fanout.js";
